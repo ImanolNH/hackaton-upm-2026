7c8fc2f986c0bf1e00c36ba083840d1dea1b776f
oscarLencero <oscar.lencero@alumnos.upm.es>
2026-03-17 13:03:04 +0100

    cambios

--- demo/src/main/java/hackatonScrumless/InterfazBackoffice.java
+++ demo/src/main/java/hackatonScrumless/InterfazBackoffice.java
@@ -5,4 +5,21 @@ public class InterfazBackoffice extends Interfaz{
     public void previsionMeteorologica(){
         super.previsionMeteorologica();
     }
-}
+    public Alerta crearAlerta(String titulo, String mensaje, NivelDeAlerta nivel, String zona){
+        Alerta nuevaAlerta = new Alerta(
+                titulo,
+                mensaje,
+                nivel,
+                "Centro de Control Meteorológico", // Emisor por defecto
+                zona
+        );
+
+        // 2. Aquí podrías añadir lógica extra antes de devolverla
+        // Ejemplo: Si es DESASTRE, forzar que el mensaje esté en mayúsculas
+        if (nivel == NivelDeAlerta.DESASTRE) {
+            nuevaAlerta.setMensaje("\u001B[31m" + nuevaAlerta.getMensaje().toUpperCase() + "\u001B[0m");
+        }
+
+        return nuevaAlerta;
+    }
+    }
